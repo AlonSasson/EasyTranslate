747bd9110a3bf80453fc0c0d63915d06c7bad39f
Alon Sasson <03alon@walla.com>
2020-12-28 00:56:56 +0200

    Added support for right to left languages and text is now devided correctly between all locations

--- TextReplacement.py
+++ TextReplacement.py
@@ -1,45 +1,88 @@
 import cv2
 import numpy
-import imutils
+import ImageProcessing as ip
+import functools
+import math
 from PIL import ImageFont, ImageDraw, Image
 
 
 def blur_locations(image, locations):
+    """ blurs all of the locations in a list in an image
+    :param image: the image in which we blur locations
+    :param locations: a list of locations
+    :return: the image with the blurred locations
+    """
     for loc in locations:
         (x, y, w, h) = loc
-        roi = image[y:y+h, x:x+w]  # seperate the roi
+        roi = image[y:y+h, x:x+w]  # separate the roi
         blur = cv2.GaussianBlur(roi, (51, 51), 0)  # apply a gaussian blur filter
         image[y:y+h, x:x+w] = blur  # insert the blurred roi back into the image
     return image
 
 
-def place_text_in_locs(image, locations, text):
-    text = 'השועל המהיר והחום קופץ מעל הכלב העצל'[::-1]
+def get_max_font_size(font_name, text, image):
+    """
+    gets the max font size using font_name and text without exceeding the image's boundries
+    :param font_name: a font name
+    :param text: the text which we try to fit in image
+    :param image: a pillow image
+    :return: the max font size in which text can fit in image perfectly
+    """
+    font_size = 1
+    font = ImageFont.truetype(font_name, font_size)
+    jump_size = 75
+    while True:  # finding the right font size using binary search
+        if font.getsize(text)[0] < image.size[0]:  # if the text font is still small
+            font_size += jump_size
+        else:  # if the text size is too big
+            jump_size = jump_size // 2
+            font_size -= jump_size
+        font = ImageFont.truetype(font_name, font_size)
+        if jump_size <= 1:  # if we found the correct font size
+            return font_size - 1  # remove one so the text still fits
+
+
+def devide_text_between_locations(words, locations):
+    """ devide the words into the locations correctly (a certain amount of words per location)
+    :param words: a list of words
+    :param locations: the locations we need to devide the words between
+    :return: the words list after deviding them correctly per location
+    """
+    words_count = len(words)
+    for i in range(len(locations)):
+        for j in range(1, math.ceil(words_count / (len(locations) - i))):  # calcualte how many words we need to join
+            words[i] += ' ' + words[i + 1]  # join the next word
+            words.remove(words[i + 1])  # delete the next word
+        words_count -= math.ceil(words_count / (len(locations) - i)) # remove the amount of words we joined from the total count
+    return words
+
+
+def place_text_in_locs(image, locations, text, right_left=False):
+    """ places text in an image in certain locations
+    :param image: the image we want to write text on
+    :param locations: the locations in which we need to write text
+    :param text: the text we need to write
+    :param right_left: a flag that determines whether the text should be written from right to left
+    :return: the processed image with the text in it
+    """
     words = text.split()
+    if right_left:  # if the text should be written from right to left
+        text = text[::-1]
+        locations = sorted(locations, key=functools.cmp_to_key(ip.cmp_locs_right_left))  # sort from right to left
+        words = text.split()[::-1]
+
+    words = devide_text_between_locations(words, locations)
     for i, loc in enumerate(locations):
         if i >= len(words):
             break
         (x, y, width, height) = loc
         roi = image[y:y+height, x:x+width]
-        roi_pil = Image.fromarray(roi)
+        roi_pil = Image.fromarray(roi)  # convert the roi and image to pillow images
         image_pil = Image.fromarray(image)
         draw = ImageDraw.Draw(image_pil)
-        font_size = 1
+        font_size = get_max_font_size("arial.ttf", words[i], roi_pil)  # get the max font size for that roi
         font = ImageFont.truetype("arial.ttf", font_size)
-        jump_size = 75
-        while True:  # finding the right font size using binary search
-            if font.getsize(words[i])[0] < roi_pil.size[0]: # if the text font is still small
-                font_size += jump_size
-            else:  # if the text size is too big
-                jump_size = jump_size // 2
-                font_size -= jump_size
-            font = ImageFont.truetype("arial.ttf", font_size)
-            if jump_size <= 1:  # if we found the correct font size
-                break
-        font_size -= 1
-        font = ImageFont.truetype("arial.ttf", font_size)
-        print(font_size)
-        draw.text((x, y - height // 5), words[i], font=font, fill=(0, 0, 0))
+        draw.text((x, y + height - font.getsize(words[i])[1]), words[i], font=font, fill=(0, 0, 0))  # draw the text in the correct location
         image = numpy.array(image_pil)
     return image
 
--- Translate.py
+++ Translate.py
@@ -1,6 +1,7 @@
 import requests
-def translate_post(word_to_translate):
+def translate_post(word_to_translate, dest_language):
+    right_left = dest_language == 'iw'
     url = 'https://www.webtran.eu/gtranslate/'
-    data_translate = {'text' : word_to_translate, 'gfrom' : 'en', 'gto' : 'iw'}
+    data_translate = {'text' : word_to_translate, 'gfrom' : 'en', 'gto' : dest_language}
     translate_words = requests.post(url, data=data_translate)
-    return translate_words.text
+    return translate_words.text, right_left
