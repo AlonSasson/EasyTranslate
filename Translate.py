c7baf0edda51faf805bf152a71decd776b43b5b7
ron sason <yafagame@gmail.com>
2020-12-28 17:39:13 +0200

    adding python lib translate

--- Translate.py
+++ Translate.py
@@ -1,4 +1,19 @@
+from translate import Translator
 import requests
+import time
+
+
+
+def translate_lib(text_to_translate, dest_language):
+    #dest_language: for Hebrew input 'Hebrew' and not 'iw'
+
+    #set setting to translate
+    translator= Translator(to_lang=dest_language, from_lang='en')
+    #translate the text
+    translation = translator.translate("The quick brown fox jumps over the lazy dog")
+
+    return translation
+
 def translate_post(word_to_translate, dest_language):
     right_left = dest_language == 'iw'
     url = 'https://www.webtran.eu/gtranslate/'
