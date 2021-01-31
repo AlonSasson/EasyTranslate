30735a37e27cda6f673ef3f567e4abc65b659133
ron sason <yafagame@gmail.com>
2021-02-01 01:12:35 +0200

    adding and desing basic of GUI

---
+++ GUI/Modern-Media-Player-UI-C-Sharp-master/PlayerUI/uploadVide.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PlayerUI
+{
+    public partial class uploadVide : Form
+    {
+        public uploadVide()
+        {
+            InitializeComponent();
+        }
+
+        private void upLoadFile_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog open = new OpenFileDialog();
+            open.Filter = "Video Files(*.avi; *.mp4;)";
+
+            if (open.ShowDialog() == DialogResult.OK)
+            {
+                textBoxPath.Text = open.FileName;
+            }
+
+
+        }
+    }
+}
